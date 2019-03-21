a0346ffae810a1db7e0c4cd2d514db4da94197b2
thopn95 <thopn95@gmail.com>
2019-03-22 00:09:34 +0700

    + add getter setter

--- src/07.class.kt
+++ src/07.class.kt
@@ -16,6 +16,23 @@ open class Person (open val firstName: String, val lastName: String, var age: In
     open fun talk(message: String = ""){
         println("$fullName says $message")
     }
+    //getter & setter
+    var isAdult: Boolean
+        get() {
+            println("Prepare to get isAdult")
+            if(age == null){
+                return false
+            }else if (age!! > 18){ // !! khac null
+                return true
+            }
+            return false
+        }
+        set(value) {
+            println("Prepare to set isAdult")
+            age = if (value == true) 18 else null
+        }
+
+    lateinit var language : String //lateinit cho phep khoi tao sau nhung khong cho phep thuoc tinh null
 }
 
 class Engineer (var fieldOfStudy: String, firstName: String, lastName: String,age: Int?):Person(firstName,lastName,age){
@@ -34,5 +51,13 @@ fun main(args: Array<String>) {
     println("Function details. FirstName = ${engineer.firstName}, LastName = ${engineer.lastName}")
     engineer.talk(" Good evening")
 
+    //getter
+    println("${engineer.firstName} is an adult ? ${engineer.isAdult}")
+    //setter
 
+//    engineer.isAdult = false
+    println("${engineer.firstName} is age ? ${engineer.age}")
+
+    engineer.language = "English"
+    println("Details. Language : ${engineer.language}")
 }
